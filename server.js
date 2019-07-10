8f3c50118b4e45768d4eba5c164718c859a3f453
Vasilev07 <georgi_vasilev07@abv.bg>
2019-07-10 22:44:07 +0300

    removed not needed file

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hi there blueprint');
-});
-
-app.listen(port, () => console.log(`Blueprint app listening on ${port}`));

d387210182b477f99c63b75578abcdc12d09c995
georgi <georgi.vasilev@synchronoss.com>
2019-07-18 20:28:30 +0300

    NOT WORKING IMPLEMENTATION OF MONGOOSE AND MONGO

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,31 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
-import { MongoClient } from 'mongodb';
+import mongoose, {Schema} from 'mongoose';
 
 const app: Application = express();
 const port = 5000;
- 
+
 app.get('/', (request: Request, response: Response, next: NextFunction) => {
   response.send('Hello world!');
 });
- 
+
+
+const mongoDb = "mongodb://127.0.0.1/test";
+
+mongoose.connect(mongoDb, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Connection error"));
+
+
+const Shark = new Schema ({
+  name: { type: String, required: true },
+  character: { type: String, required: true },
+});
+const model = mongoose.model('Shark', Shark);
+const test = new model({
+  name: 'TEST',
+  character: 'IT IS WORKING'
+}) as Partial<Document>
+
 app.listen(port, () => console.log(`Listening on ${port}`));
 
 
-(async () => {
-    const connection = await MongoClient.connect('mongodb://localhost:27017/test');
-    const db = connection.db('test');
-})();
